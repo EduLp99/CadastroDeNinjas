8c80ce6a939d2ecfb59e6468d7ea158b8b311bac
EduLp99 <edulp99@gmail.com>
2025-05-13 01:58:15 -0300

    Modelagem do banco de dados, ninjaModel e missionModel

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissionModel.java
@@ -0,0 +1,20 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+public class MissionModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String missionName;
+    private String difficulty;
+    //UMA MISSAO PODE TER VARIOS NINJAS
+    @OneToMany(mappedBy = "mission")
+    private List<NinjaModel> ninjas;
+}
